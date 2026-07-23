61c27224a7697aae401730083b35eedee75706e6
Chelennisa Djaka I.R XRPL6 <chelennisadjaka.ir@gmail.com>
2026-07-23 08:10:37 +0700

    Add files via upload

---
+++ praktikumSoal-1.js
@@ -0,0 +1,7 @@
+let skor = '20xp';
+let level = '8';
+let nyawa = '20HP';
+
+const namaPemain = 'selen';
+
+console.log (`character ku bernama ${namaPemain}, skornya bertotal +${skor} karna telah menyelesaikan satu tugas, menyisakan ${nyawa} dan telah mencapai level ${level}`)
