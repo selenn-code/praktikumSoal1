a987bf46b541f6cd9dc6e795ceaa1323e0c89a5f
Chelennisa Djaka I.R XRPL6 <chelennisadjaka.ir@gmail.com>
2026-07-23 08:18:15 +0700

    Add files via upload

--- praktikumSoal-1.js
+++ praktikumSoal-1.js
@@ -7,3 +7,5 @@ const namaPemain = 'selen';
 console.log (`character ku bernama ${namaPemain}`)
 console.log (`skornya bertotal +${skor} karna telah menyelesaikan satu tugas`)
 console.log (`menyisakan ${nyawa} dan telah mencapai level ${level}`)
+
+namaPemain = 'frea'
